85706d401623f50f96eef33073f3cb3d47719cc0
Austin Peete <apeete95@gmail.com>
2016-01-11 19:04:44 -0500

    160102: Awww ya! Makin progress

---
+++ sql.py
@@ -0,0 +1,19 @@
+##### MODEL ##### 
+#Create a SQLite3 table and populate it with data
+
+import sqlite3
+
+#create a new db if the db doesn't already exist
+with sqlite3.connect("blog.db") as connection:
+    c = connection.cursor()
+
+    #create table
+    c.execute("""CREATE TABLE posts
+                (title TEXT, post TEXT)
+                """)
+
+    #insert dummy data into table
+    c.execute('INSERT INTO posts VALUES("Good", "I\'m good.")')
+    c.execute('INSERT INTO posts VALUES("Well", "I\'m well")')
+    c.execute('INSERT INTO posts VALUES("Excellent", "I\'m Excellent")')
+    c.execute('INSERT INTO posts VALUES("Okay", "I\'m okay")')
